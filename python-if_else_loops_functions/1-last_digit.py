fbe058712579f3fb929b2e2a83976bbf69b95849
Khadaassi <aassikhadija@gmail.com>
2024-01-22 13:23:25 +0100

    Fix last_digit calculation in python-if_else_loops_functions/1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+
 if number < 0:
     last_digit = number % -10
-elif:
+elif number >= 0:  # Add condition and colon
     last_digit = number % 10
 else:
     last_digit = 0
+
 if last_digit > 5:
     print("Last digit of", number, "is", last_digit, "and is greater than 5")
 elif last_digit == 0:
