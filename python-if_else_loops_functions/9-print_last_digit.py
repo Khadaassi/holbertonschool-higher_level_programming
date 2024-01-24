442c6d7ef7818e2a123ba9fbe1581f4bb5d624be
Khadaassi <aassikhadija@gmail.com>
2024-01-24 15:43:02 +0000

    Fix print_last_digit function to correctly handle negative numbers

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+
 def print_last_digit(number):
+
     if number < 0:
         number = -number
+        
     print("{:d}".format(number % 10), end="")
     return number % 10
