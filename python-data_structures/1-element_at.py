eb9c9faa40aa42c7a1c3983077eb5c6ed586ba10
Khadaassi <aassikhadija@gmail.com>
2024-01-29 12:42:24 +0000

    Fix index validation in element_at function

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0 and idx > len(my_list):
+    if idx != int(idx):
+        return None
+    if idx < 0:
+        return None
+    elif idx > len(my_list):
         return None
     else:
         return my_list[idx]
