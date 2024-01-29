c71b5f1683274748009fe75519b33bfe88a3297d
Khadaassi <aassikhadija@gmail.com>
2024-01-29 12:51:58 +0000

    Fix index out of range bug in element_at, replace_in_list, and new_in_list functions

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@
 def new_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    elif idx > len(my_list):
+    elif idx > (len(my_list) - 1):
         return my_list
     else:
         new_list = my_list[:]
