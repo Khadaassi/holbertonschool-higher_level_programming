9882a3ec8752d10eed585d937919facfb57efdfc
Khadaassi <aassikhadija@gmail.com>
2024-01-30 09:57:50 +0000

    Fix pycodestyle trailing whitespace

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,4 +4,4 @@ def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
         new_matrix.append(list(map(lambda x: x**2, row)))
-    return new_matrix   
+    return new_matrix
