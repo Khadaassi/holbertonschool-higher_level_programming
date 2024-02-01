adb75b6e9a62651991d9a9c489f70dfd3c671b56
Khadaassi <aassikhadija@gmail.com>
2024-02-01 17:49:24 +0100

    Add exception handling to safe_print_integer, safe_print_division, and list_division functions

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,14 +1,18 @@
-#!/usr/bin/python3
-list_division = __import__('4-list_division').list_division
-
-my_l_1 = [10, 8, 4]
-my_l_2 = [2, 4, 4]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
-
-print("--")
-
-my_l_1 = [10, 8, 4, 4]
-my_l_2 = [2, 0, "H", 2, 7]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
+#!usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            result.append(my_list_1[i] / my_list_2[i])
+        except TypeError:
+            print("wrong type")
+            result.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            result.append(0)
+        except IndexError:
+            print("out of range")
+            result.append(0)
+        finally:
+            pass
+    return result
