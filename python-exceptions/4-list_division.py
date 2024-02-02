c0ad362c325d7b13453b2b2720ef0a43af24cceb
Khadaassi <aassikhadija@gmail.com>
2024-02-02 10:56:47 +0100

    fixing list_division function to handle exceptions and store results in a new list

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,18 +1,22 @@
 #!usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    result = []
+    new_list = []
+
     for i in range(list_length):
         try:
-            result.append(my_list_1[i] / my_list_2[i])
-        except TypeError:
-            print("wrong type")
-            result.append(0)
+            result = my_list_1[i] / my_list_2[i]
+
         except ZeroDivisionError:
             print("division by 0")
-            result.append(0)
+            result = 0
+
+        except TypeError:
+            print("wrong type")
+            result = 0
+
         except IndexError:
             print("out of range")
-            result.append(0)
+            result = 0
+
         finally:
-            pass
-    return result
+            new_list.append(result)
