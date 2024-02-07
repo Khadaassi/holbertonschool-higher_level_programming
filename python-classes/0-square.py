2d58cb3ff222449b1c91fae4abae94715e30a196
Khadaassi <aassikhadija@gmail.com>
2024-02-07 18:24:27 +0100

    Refactor Square class initialization

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-class Square:
-    """Represents a square.
+"""Represents a square.
     Private instance attribute: size.
     Instantiation with size (no type/value verification).
     """
-
+class Square:
     def __init__(self, size):
         """Initializes the data."""
         self.__size = size
