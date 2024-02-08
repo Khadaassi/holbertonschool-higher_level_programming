043e40294e5810ab979d32919610212ea3067e1e
Khadaassi <aassikhadija@gmail.com>
2024-02-08 10:32:56 +0100

    Refactor Square class initialization and add docstring

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+
+"""class Square that defines a square by: (based on 2-square.py)"""
+
+
 class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with optional size.
-    Public instance method: def area(self).
-    """
 
     def __init__(self, size=0):
         """Initializes the data."""
