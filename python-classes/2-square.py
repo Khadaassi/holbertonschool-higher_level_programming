c3f7142dd2e7d5042ea657417c9bf718078fb46f
Khadaassi <aassikhadija@gmail.com>
2024-02-08 10:31:07 +0100

    Refactor Square class constructor to validate size parameter

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,10 +5,12 @@
 
 class Square:
 
-    def __init__(self, size=0):
+    """ Square class that define a square with is size"""
 
-        if not isinstance(size, int):
+    def __init__(self, size=0):
+        if not type(size) is int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
+        """square size private instance attribute"""
         self.__size = size
