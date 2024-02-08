09b024e564195dc2d7d402a93db8a76d26445ff3
Khadaassi <aassikhadija@gmail.com>
2024-02-08 10:26:48 +0100

    Refactor Square class initialization

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,8 +7,9 @@
 
 
 class Square:
+
     def __init__(self, size=0):
-        """Initializes the data."""
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
