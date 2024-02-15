e001d1dcc6ba80262d7f425871685e6a4ffc2bca
Khadaassi <aassikhadija@gmail.com>
2024-02-15 15:07:23 +0100

    Refactor Rectangle class to use integer_validator for width and height

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,27 +1,7 @@
 #!/usr/bin/python3
 """ Module for is_kind_of_class method"""
 
-
-class BaseGeometry:
-    """
-    Class BaseGeometry
-    """
-
-    def area(self):
-        """
-        Method that raises an Exception with message area() is not implemented
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Method that validates value
-        """
-        if value not in [int]:
-            raise TypeError(name + "must be an integer")
-
-        if value <= 0:
-            raise ValueError(name + "must be greater than 0")
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -32,3 +12,6 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         self.__width = width
         self.__height = height
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
