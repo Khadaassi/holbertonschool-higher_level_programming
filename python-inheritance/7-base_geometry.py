0bc5f0a032a9de8af01aa871f225e99581f7bfb8
Khadaassi <aassikhadija@gmail.com>
2024-02-15 14:06:31 +0100

    Fix error message in BaseGeometry class

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -19,7 +19,7 @@ class BaseGeometry:
         """
 
         if type(value) not in [int]:
-            raise TypeError("<name> must be an integer")
+            raise TypeError(name + " must be an integer")
 
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError(name + " must be greater than 0")
