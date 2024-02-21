2a4f7f15c6ad802653c5dc4a38d163b9132d7eae
Khadaassi <aassikhadija@gmail.com>
2024-02-21 14:44:55 +0100

    pycodestyle fixed

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -25,7 +25,8 @@ class Square(Rectangle):
         String representation of the square
         """
 
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format(self.id,
+                                                 self.x, self.y, self.width)
 
     @property
     def size(self):
