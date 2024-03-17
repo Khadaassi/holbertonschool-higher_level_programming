dfe035986a5570ab5c89efef78fc8bceab748532
Khadaassi <aassikhadija@gmail.com>
2024-03-17 11:20:12 +0100

    Fix table creation and add records to second_table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,10 +1,11 @@
--- creates a table second_table in the database 
-
-CREATE TABLE IF NOT EXIST second_table (
+-- creates a table second_table in the database hbtn_0c_0
+CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
-    );
+);
+
+-- create these records into the 'second_table'
 INSERT INTO second_table (id, name, score) VALUES
 (1, 'John', 10),
 (2, 'Alex', 3),
