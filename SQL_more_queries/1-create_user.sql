8048f8991e61b2af37466a1a7dba5fef116d1bc5
Khadaassi <aassikhadija@gmail.com>
2024-03-18 11:04:48 +0100

    Fix user creation query to handle existing users

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creates the MySQL server user 
 
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
