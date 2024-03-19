70f2b25dcd7cf0b45056967e62c2ed72dbc772d0
Khadaassi <aassikhadija@gmail.com>
2024-03-19 12:15:00 +0100

    Add SQL queries for TV shows and genres

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- lists all shows, and all genres linked to that show
+SELECT tv_shows.title, tv_genres.name
+FROM tv_show_genres
+RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name;
